22d6bac4046d05ade652d95b58bcb1fdcd2d2b63
dldnwls0121 <alsdus0907@naver.com>
2025-08-06 15:45:53 +0900

    25.08.06 선택 정렬

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,9 +1,60 @@
 ﻿#include <iostream>
-
+#include <cmath>
 using namespace std;
 
+void sieve(int n)
+{
+	int* array = new int[n];
+	int j = 1;
+	for (int i = 0; i < n; i++)
+	{
+		array[i] = j;
+		j++;
+	}
+
+	int count = 0;
+	for (int i = 0; i < sqrt(array[i]); i++)
+	{
+		for (count = 2;  count< length; count++)
+		{
+
+		}
+	
+		
+
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << array[i];
+
+	}
+
+	delete[] array;
+}
 int main()
 {
+#pragma region 에라토스테네스의 체
+	int j = 0;
+	for (int i = 2; i <= 50; i++)
+	{
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0 )
+			{
+				break;
+			}
+		}
+
+		if (i == j)
+		{
+			//cout << i << endl;
+		}
+	
+	}
+
+	sieve(16);
+#pragma endregion
+
 
 }
 
