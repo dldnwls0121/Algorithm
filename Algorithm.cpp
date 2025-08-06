d7c476a08adf3e9dfbd8308aed5475cd68bb1d62
dldnwls4148 <dldnwls4148@naver.com>
2025-08-06 23:19:20 +0900

    저장

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -4,30 +4,31 @@ using namespace std;
 
 void sieve(int n)
 {
-	int* array = new int[n];
-	int j = 1;
-	for (int i = 0; i < n; i++)
-	{
-		array[i] = j;
-		j++;
-	}
+	int* array = new int[n + 1];
 
-	int count = 0;
-	for (int i = 0; i < sqrt(array[i]); i++)
-	{
-		for (count = 2;  count< length; count++)
-		{
+    for (int i = 0; i < n; i++)
+        array[i] = i + 1;
 
-		}
-	
-		
+    array[0] = 0; 
 
-	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << array[i];
+    for (int i = 1; i < n; i++)
+    {
+        if (array[i] != 0 && array[i] <= (int)sqrt(n))
+        {
+            int prime = array[i];
+            for (int j = prime * prime - 1; j < n; j += prime)
+            {
+                array[j] = 0; 
+            }
+        }
+    }
 
-	}
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] != 0)
+            cout << array[i] << " ";
+    }
+    cout << endl;
 
 	delete[] array;
 }
