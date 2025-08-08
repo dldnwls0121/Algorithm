e53431514d13636099664623a8eef75636a5026c
dldnwls0121 <alsdus0907@naver.com>
2025-08-08 14:03:38 +0900

    25.8.8 계수 정렬

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,42 +1,88 @@
 ﻿#include <iostream>
-
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-int find(int array[],int start,int end,int key)
-{
-	if (start > end)
-	{
-		cout << "키를 찾을 수 없음" << endl;
-		return 0;
-	}
 
-	int pivot = (start + end) / 2;
-	if (array[pivot] == key)
-	{
-		cout << key << endl;
-		return key;
-	}
-	else if(key < array[pivot])
-	{
-		int Left_v = find(array, start, pivot - 1, key);
-	}
-	else
-	{
-		int Right_v = find(array, pivot + 1, end, key);
-	}
-	
-	return 0;
-}
 int main()
 {
-#pragma region 이분 탐색
-	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를 좁혀나가는 방식으로 동작하는 알고리즘
-	int array[] = { 6,10,13,22,57 };
-	int size = sizeof(array) / 4 - 1;
-	find(array, 0, size, 22);
+#pragma region 계수 정렬
+	// 데이터의 값을 비교하지 않고 각 원소에 데이터가 몇 개 있는지
+	// 개수를 세어 저장한 다음 정렬하는 알고리즘
 
+	int array[] = {1,3,2,2,4,5,1,2};
+	int len = sizeof(array) / sizeof(array[0]);
+	//vector<int> count(6,0);
+	//vector<int> answer;
+	//for (int i = 0; i < len; i++)
+	//{
+	//	if (array[i] == 1)
+	//	{
+	//		count[array[i]]++;
+	//	}
+	//	else if (array[i] == 2)
+	//	{
+	//		count[array[i]]++;
+	//	}
+	//	else if (array[i] == 3)
+	//	{
+	//		count[array[i]]++;
+	//	}
+	//	else if (array[i] == 4)
+	//	{
+	//		count[array[i]]++;
+	//	}
+	//	else if (array[i] == 5)
+	//	{
+	//		count[array[i]]++;
+	//	}
+	//
+	//}
+	//
+	//for (int i = 1; i <= 5; i++)
+	//{
+	//	for (int j = 0; j < count[i]; j++)
+	//	{
+	//		answer.push_back(i);
+	//	}
+	//}
+	//for (auto& a : answer)
+	//{
+	//	cout << a << endl;
+	//}
 
+	unordered_map<int, int> count;
+
+	for (int i = 0; i < len; i++)
+	{
+		count[array[i]]++;
+	}
+
+	vector<int> key;
+	for (auto& a : count)
+	{
+		key.push_back(a.first);
+	}
+
+	sort(key.begin(), key.end());
+
+	vector<int> answer;
+	for (int a : key)
+	{
+		for (int i = 0; i < count[a]; i++)
+		{
+			answer.push_back(a);
+		}
+		
+	}
+	
+	for (int a : answer)
+	{
+		cout << a << endl;
+	}
 #pragma endregion
+
 
 
 }
---
+++ Class9th (Binary Search)/Class9th (Binary Search).cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int find(int array[], int start, int end, int key)
+{
+	if (start > end)
+	{
+		cout << "키를 찾을 수 없음" << endl;
+		return 0;
+	}
+
+	int pivot = (start + end) / 2;
+	if (array[pivot] == key)
+	{
+		cout << key << endl;
+		return key;
+	}
+	else if (key < array[pivot])
+	{
+		int Left_v = find(array, start, pivot - 1, key);
+	}
+	else
+	{
+		int Right_v = find(array, pivot + 1, end, key);
+	}
+
+	return 0;
+}
+int main()
+{
+#pragma region 이분 탐색
+	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를 좁혀나가는 방식으로 동작하는 알고리즘
+	int array[] = { 6,10,13,22,57 };
+	int size = sizeof(array) / 4 - 1;
+	find(array, 0, size, 22);
+
+
+#pragma endregion
+
+
+}
+
